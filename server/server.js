dc3b5af120c51787aecf1fbcd88d7b70950ab873
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2020-12-27 00:07:52 +0200

    added mongoose

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 
 dotenv.config()
+connectDB()
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('<h1 style="color:red;">hello mfs</h1>')
+    res.send(`<h1 style="color:red;">${process.env.NODE_ENV}</h1>`)
 })
 
 app.get('/api/products', (req, res) => {
@@ -24,7 +27,5 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
     PORT,
-    console.log(`Running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`>>> Running in ${process.env.NODE_ENV} mode on port: ${PORT.yellow.underline}`)
 )
-
-
