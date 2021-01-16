a80340ee4a1142fa008ae6ec88b31b62f49f5ee0
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2021-01-16 22:41:38 +0200

    added middleware for errors

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 
 dotenv.config()
@@ -11,14 +12,41 @@ const app = express()
 
 app.get('/', (req, res) => {
     res.send(`
-        <h1 style="color:red;">
-            ${process.env.NODE_ENV}
-        </h1>
+        <div style="text-align: center;">
+            <h1 style="color: red;">${process.env.NODE_ENV}</h1>
+            <div style="cursor: default; font-size: 2rem;">
+                <div>░░░░░░░░░░░░░░░░░░░░░░░░░░█▄</div>
+                <div>░▄▄▄▄▄▄░░░░░░░░░░░░░▄▄▄▄▄░░█▄</div>
+                <div>░▀▀▀▀▀███▄░░░░░░░▄███▀▀▀▀░░░█▄</div>
+                <div>░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█</div>
+                <div>░▄▀▀▀▀▀▄░░░░░░░░░░▄▀▀▀▀▀▄░░░░█</div>
+                <div>█▄████▄░▀▄░░░░░░▄█░▄████▄▀▄░░█▄</div>
+                <div>████▀▀██░▀▄░░░░▄▀▄██▀█▄▄█░█▄░░█</div>
+                <div>██▀██████░█░░░░█░████▀█▀██░█░░█</div>
+                <div>████▀▄▀█▀░█░░░░█░█████▄██▀▄▀░░█</div>
+                <div>███████▀░█░░░░░░█░█████▀░▄▀░░░█</div>
+                <div>░▀▄▄▄▄▄▀▀░░░░░░░░▀▀▄▄▄▄▀▀░░░░░█</div>
+                <div>░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█</div>
+                <div>░░▓▓▓▓▓▓▓░░░░░░░░░░▓▓▓▓▓▓▓░░░░█</div>
+                <div>░░░▓▓▓▓▓░░░░░░░░░░░░▓▓▓▓▓░░░░░█</div>
+                <div>░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█</div>
+                <div>░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█▀</div>
+                <div>░░░░░░░░░▄▄███████▄▄░░░░░░░░░█</div>
+                <div>░░░░░░░░█████████████░░░░░░░█▀</div>
+                <div>░░░░░░░░░▀█████████▀░░░░░░░█▀</div>
+                <div>░░░░░░░░░░░░░░░░░░░░░░░░░░█▀</div>
+                <div>░░░░░░░░░░░░░░░░░░░░░░░░░█▀</div>
+            </div>
+        </div>
     `)
 })
 
 
 app.use('/api/products', productRoutes)
+
+// middleware
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
